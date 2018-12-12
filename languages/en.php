29cdf560ef8b60d2d48961e7a5d157d97a777f71
DavidZgz32 <david_zgz_1@hotmail.com>
2018-12-12 19:51:36 +0100

    Add files via upload

---
+++ languages/en.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang = array(
+    "title" => "Admin panel",
+    "heading1" => "Add data",
+    "lang_en" => "English",
+    "lang_es" => "Spanish"
+);
---
+++ languages/es.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang = array(
+    "title" => "Panel de administración",
+    "heading1" => "Añadir datos",
+    "lang_en" => "Inglés",
+    "lang_es" => "Español"
+);
+
